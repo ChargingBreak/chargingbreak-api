3336bf69703e83ace84995e57c9b3cce339a417c
Mike Nestor <mnestor79@gmail.com>
2018-07-13 17:25:58 -0400

    cleanup for charger method with better error support

--- src/charger.py
+++ src/charger.py
@@ -2,6 +2,7 @@ import json
 import os
 
 import boto3
+from src import decimalencoder as de
 
 
 def post(event, context):
@@ -17,10 +18,31 @@ def get(event, context):
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table(os.environ['SUPERCHARGERINFO_TABLE'])
 
-    if 'path' in event and 'id' in event['path']:
-        item = table.get_item(Key={'id': int(event['path']['id'])})
+    body = "Unknown Error"
+    statusCode = 200
 
-    return item['Item']
+    try:
+        if ('pathParameters' in event
+            and event['pathParameters']
+                and 'id' in event['pathParameters']):
+            item = table.get_item(
+                Key={'id': int(event['pathParameters']['id'])})
+            body = json.dumps(item['Item'], cls=de.DecimalEncoder)
+        else:
+            statusCode = 510  # not extended.
+            # Further extensions to the request are required
+            # for the server to fulfill it.
+
+            body = json.dumps(
+                {'message': 'unable to fulfill request at this time'})
+    except Exception as e:
+        body = json.dumps({'message': str(e)})
+        statusCode = 500
+
+    return {
+        "statusCode": statusCode,
+        "body": body
+    }
 
 
 methods = {
@@ -30,7 +52,7 @@ methods = {
 
 
 def main(event, context):
-    if 'method' in event:
-        return methods[event['method']](event, context)
+    if 'httpMethod' in event:
+        return methods[event['httpMethod']](event, context)
 
     return {"statusCode": 404}
